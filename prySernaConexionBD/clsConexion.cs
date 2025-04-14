c6eb81ac0457376e567ff0ed4bf867b2c1ef674f
palomaserna <palomaserna55@gmail.com>
2025-04-14 10:43:47 -0300

    conexion

---
+++ prySernaConexionBD/clsConexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySernaConexionBD
+{
+    internal class clsConexion
+    {
+        //cadena de conexion
+        string cadenaConexion = "Server=localhost;Database=Comercio;Trusted_Connection=True;";
+
+
+        //conector
+        SqlConnection coneccionBaseDatos;
+
+        //comando
+        SqlCommand comandoBaseDatos;
+
+        public string nombreBaseDeDatos;
+
+
+        public void ConectarBD()
+        {
+            try
+            {
+                coneccionBaseDatos = new SqlConnection(cadenaConexion);
+
+                nombreBaseDeDatos = coneccionBaseDatos.Database;
+
+                coneccionBaseDatos.Open();
+
+                MessageBox.Show("Conectado a " + nombreBaseDeDatos);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Tiene un errorcito - " + error.Message);
+            }
+
+        }
+    }
+}
---
+++ prySernaConexionBD/frmIncio.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace prySernaConexionBD
+{
+    partial class frmIncio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.dgvMostrar = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(30, 26);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 23);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            // 
+            // dgvMostrar
+            // 
+            this.dgvMostrar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMostrar.Location = new System.Drawing.Point(30, 55);
+            this.dgvMostrar.Name = "dgvMostrar";
+            this.dgvMostrar.Size = new System.Drawing.Size(585, 362);
+            this.dgvMostrar.TabIndex = 1;
+            // 
+            // frmIncio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvMostrar);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "frmIncio";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostrar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.DataGridView dgvMostrar;
+    }
+}
+
